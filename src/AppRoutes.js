fa36680296ee1747195cf78476717fd67dc9f272
Bikramjit Roy <96054843+Bikramjit-1@users.noreply.github.com>
2022-06-20 20:15:53 +0530

    Initial commit

---
+++ src/AppRoutes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { BackgroundColorChanger } from './Pages/BackgroundColorChanger'
+import { Calculator } from './Pages/Calculator'
+import { Index } from './Pages/Index'
+
+export const AppRoutes = () =>{
+    return (
+
+    )
+}
